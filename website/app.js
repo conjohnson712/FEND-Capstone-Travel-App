f5f7f6603bf4a188ec08d0b647938e6918870681
conjohnson712 <conjohnson712@gmail.com>
2022-03-23 18:51:54 -0400

    Added API key and starter code

--- website/app.js
+++ website/app.js
@@ -1,5 +1,26 @@
 /* Global Variables */
 
+// Personal API Key for OpenWeatherMap API
+// References: 
+// Lesson 4-6: https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1846/concepts/211c2a41-4ab7-48ea-94cc-b44b2e4363c4
+// Knowledge Post: https://knowledge.udacity.com/questions/771226
+let baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey ="a29269f9dfa285042b1085ced6cbfb7b&units=imperial";
+
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Event listener to add function to existing HTML DOM element
+
+
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+
+/* Function to GET Project Data */
