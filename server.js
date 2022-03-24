88cfba7a582a83c9827627973cca1e0f51de7f46
conjohnson712 <conjohnson712@gmail.com>
2022-03-24 19:17:18 -0400

    95% Finished. Some Errors to fix before completion

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ projectData = {};
 
 // Express to run server and routes
 // Reference: Lesson 2-4 https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1844/concepts/f9133dcc-e746-4bbe-a5f5-91941535940d
-const express = require('express');
+const express = require("express");
 
 // Start up an instance of app
 // Reference: Lesson 2-4 https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1844/concepts/f9133dcc-e746-4bbe-a5f5-91941535940d
@@ -11,7 +11,7 @@ const app = express();
 
 /* Dependencies */
 // Reference: Lesson 2-4 https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1844/concepts/f9133dcc-e746-4bbe-a5f5-91941535940d
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser")
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -21,12 +21,12 @@ app.use(bodyParser.json());
 
 // Cors for cross origin allowance
 // Reference: Lesson 2-4 https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1844/concepts/f9133dcc-e746-4bbe-a5f5-91941535940d
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
 // Initialize the main project folder
 // Reference: Lesson 2-8: https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1844/concepts/ecf2408b-6ab1-4906-bd28-8348d99bc95d
-app.use(express.static('website'));
+app.use(express.static("website"));
 
 const port = 8712;
 
@@ -44,7 +44,7 @@ const server = app.listen(port, listening);
 // Initialize all route with a callback function
 // Callback function to complete GET '/all'
 // Reference: Lesson 3-2: https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1845/concepts/b0d68e7d-274a-43ef-b3da-3cfe93a77961
-app.get('/all', (req, res)=>{
+app.get("/weatherData", (req, res)=>{
     res.send(projectData);
 });
 
@@ -54,13 +54,13 @@ app.get('/all', (req, res)=>{
 // Lesson 3-5: https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1845/concepts/0c75d5b8-3dde-4404-9552-c1c76c10b2ab
 // Lesson 3-8: https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1845/concepts/81afa555-a670-428e-99a2-3a4d3ccefc96
 // Lesson 3-9: https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1845/concepts/f0b46126-a01c-43c9-8431-9e9e6ae4d85d
-app.post('/weatherData', addWeatherData);
+app.post("/weatherData", addWeatherData);
 
 // addWeatherData: request, response --> void
 // Verifies a Post was received, then builds newJournal entries with 
 // the weather data
 function addWeatherData (req, res){
-    res.send('POST received');
+    res.send("POST received");
     let newData = req.body;
     let newJournal = {
         date: newData.date,
--- website/app.js
+++ website/app.js
@@ -4,26 +4,37 @@
 // References: 
 // Lesson 4-6: https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1846/concepts/211c2a41-4ab7-48ea-94cc-b44b2e4363c4
 // Knowledge Post: https://knowledge.udacity.com/questions/771226
-let baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
-let apiKey ="b6a391b79acd9df2db1fbbabbe00b992";
+const baseURL = "https://api.openweathermap.org/data/2.5/weather?zip=";
+const apiKey ="&appid=b6a391b79acd9df2db1fbbabbe00b992&units=imperial";
 
 
 // Create a new date instance dynamically with JS
 // Part of starter code
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 /* Function called by event listener */
-// Reference: Lesson 4-6: https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1846/concepts/211c2a41-4ab7-48ea-94cc-b44b2e4363c4
+// References: 
+// Lesson 4-6: https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1846/concepts/211c2a41-4ab7-48ea-94cc-b44b2e4363c4
+// Lesson 4-10: https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1846/concepts/06b6f9e9-221f-4668-8d13-a70346b293d2
 // generateJournal: element --> void
-// Gets and assigns values needed to generate a journal entry.
-// Calls upon the getWeatherData function
+// Gets and assigns values needed to generate a journal entry (zip, feelings, date).
+// Calls the getWeatherData function
 const generateJournal = (e) => {
     const zipCode = document.getElementById("zip").value;
     const feelings = document.getElementById("feelings").value;
+
     console.log(newDate);
     getWeatherData(baseURL, zipCode, apiKey)
-}
+    .then(function(data){
+        // Add data
+        console.log(data);
+        postData("http://localhost:8712/weatherData", {temp: data.temp, date: newDate, content: feelings})
+    })
+        .then(function(){
+            retrieveData()
+        })
+    };
 
 // Event listener to add function to existing HTML DOM element
 // This was put after generateJournal to satisfy DOM error code
@@ -31,15 +42,12 @@ const generateJournal = (e) => {
 document.getElementById("generate").addEventListener("click", generateJournal);
 
 
-
-
-
 /* Function to GET Web API Data*/
 // Reference: Lesson 4-6: https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1846/concepts/211c2a41-4ab7-48ea-94cc-b44b2e4363c4
 // getWeatherData: baseURL, zipCode, feelings, apiKey --> void
 // Async function that fetches the URL and necessary data and logs it
 const getWeatherData = async (baseURL, zipCode, apiKey) =>{
-    const res = await fetch(baseURL+zipCode+apiKey)
+    const res = await fetch(baseURL+zipCode+",us"+apiKey)
     try {
         const data = await res.json();
         console.log(data)
@@ -51,7 +59,7 @@ const getWeatherData = async (baseURL, zipCode, apiKey) =>{
 
 /* Function to POST data */
 // Reference: Lesson 4-11: https://classroom.udacity.com/nanodegrees/nd0011/parts/cd0429/modules/d153872b-b417-4f32-9c77-d809dc21581d/lessons/ls1846/concepts/9e29ea8c-f587-4e11-9c4a-e2671c23e4e8
-const postData = async ( url ="http://localhost:8712/weatherData", data = {}) =>{
+const postData = async ( url ="", data = {}) =>{
     const response = await fetch(url, {
         method: "POST",
         credentials: "same-origin",
@@ -72,14 +80,14 @@ const postData = async ( url ="http://localhost:8712/weatherData", data = {}) =>
 /* Function to GET Project Data */
 // Reference: Rubric, 'Dynamically Update UI': https://review.udacity.com/#!/rubrics/4671/view
 const retrieveData = async () =>{
-    const request = await fetch("/all");
+    const request = await fetch("/weatherData");
     try {
         // Transform into JSON
         const allData = await request.json()
         console.log(allData)
         // Write updated data to DOM Elements
         document.getElementById("temp").innerHTML = Math.round(allData.temp)+ "degrees";
-        document.getElementById("content").innerHTML = allData.feel;
+        document.getElementById("content").innerHTML = allData.content;
         document.getElementById("date").innerHTML = allData.date;
     }
     catch(error) {
