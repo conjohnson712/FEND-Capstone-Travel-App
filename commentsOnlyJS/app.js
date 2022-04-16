6d217923e4cb3bdebcbe57e4c0c51acc0616203d
Connor Johnson <80295208+conjohnson712@users.noreply.github.com>
2022-04-16 17:33:24 -0400

    Initial Commit
    My finished "Weather Journal App" project serves as the starter code for this project. The starter code for the Weather Journal was provided by Udacity.

---
+++ commentsOnlyJS/app.js
@@ -0,0 +1,17 @@
+// Personal API Key for OpenWeatherMap API
+
+
+// Create a new date instance dynamically with JS
+
+// Event listener to add function to existing HTML DOM element
+
+
+
+/* Function called by event listener */
+
+/* Function to GET Web API Data*/
+
+/* Function to POST data */
+
+
+/* Function to GET Project Data */
