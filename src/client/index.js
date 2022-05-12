2b667494025b9a005e23ad7e229735bee67bdeb9
conjohnson712 <conjohnson712@gmail.com>
2022-05-12 00:22:29 -0400

    Updated SCSS files. Added background image, but still not functional

--- src/client/index.js
+++ src/client/index.js
@@ -4,7 +4,11 @@ import { checkForCity } from './js/cityChecker';
 import { handleSubmit } from './js/formHandler';
 
 
-import './styles/base.scss'
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/header.scss';
+import './styles/resets.scss';
 
 export { 
     checkForCity,
